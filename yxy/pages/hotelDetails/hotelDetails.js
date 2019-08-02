942291b4e07ddd75ca69473550d1c7c2e0b9ff42
yuyu <1912850912@qq.com>
2019-08-02 10:32:23 +0800

    2019/8/2

--- yxy/pages/hotelDetails/hotelDetails.js
+++ yxy/pages/hotelDetails/hotelDetails.js
@@ -17,64 +17,76 @@ Page({
     duration: 1000,
   },
   //回到顶部
-  goTop: function (e) {  // 一键回到顶部
+  goTop: function(e) { // 一键回到顶部
     app.goTop();
+  },
+  //弹窗打开规则提示
+  showRule: function() {
+    console.log("show")
+    this.setData({
+      isRuleTrue: true
+    })
+  },
+  // 弹窗点击关闭按钮：
+  hideRule: function() {
+    console.log("hide")
+    this.setData({
+      isRuleTrue: false,
+    })
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    // wx.setNavigationBarTitle({
-    //   title: ""
-    // })
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
